a20cd9b2708a49b6287e3d0da91198796656f430
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-05-17 22:40:30 +0200

    initial commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Davide Castaldi on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
