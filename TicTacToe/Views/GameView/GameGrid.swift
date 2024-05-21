920f787aeef7f1c9546ff219fe89e79d454d0971
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-05-21 15:46:12 +0200

    Adjustment:
    - Now the item position is better

--- TicTacToe/Views/GameView/GameGrid.swift
+++ TicTacToe/Views/GameView/GameGrid.swift
@@ -18,34 +18,46 @@ struct GameGrid: View {
     
     var body: some View {
         ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 350, height: 350)
+                .foregroundStyle(.yellow)
             VStack(spacing: 105) {
-                Rectangle().frame(height: 5)
-                Rectangle().frame(height: 5)
+                Rectangle()
+                    .frame(height: 5)
+                Rectangle()
+                    .frame(height: 5)
             }
             .frame(width: 350, height: 100)
             
             HStack(spacing: 125) {
-                Rectangle().frame(width: 5)
-                Rectangle().frame(width: 5)
+                Rectangle()
+                    .frame(width: 5)
+                Rectangle()
+                    .frame(width: 5)
             }
             .frame(width: 100, height: 340)
             
-            LazyVGrid(columns: col, spacing: 10) {
-                ForEach(0..<9) { value in
-                    Button {
-                        gameLogic.buttonTap(index: value)
-                    } label: {
-                        TimelineView(.animation) { tl in
-                            let time = start.distance(to: tl.date)
-                            Image(gameLogic.buttonLabel(index: value))
-                                .interpolation(.none)
-                                .resizable()
-                                .frame(width: 100, height: 100, alignment: .center)
-                                .colorEffect(ShaderLibrary.rainbow(.float(time)))
+            VStack(spacing: 30) { 
+                            ForEach(0..<3) { row in
+                                HStack(spacing: 40) { // Horizontal spacing between images in a row
+                                    ForEach(0..<3) { col in
+                                        let index = row * 3 + col
+                                        Button {
+                                            gameLogic.buttonTap(index: index)
+                                        } label: {
+                                            TimelineView(.animation) { tl in
+                                                let time = start.distance(to: tl.date)
+                                                Image(gameLogic.buttonLabel(index: index))
+                                                    .interpolation(.none)
+                                                    .resizable()
+                                                    .frame(width: 80, height: 80) // Adjust size as needed
+                                                    .colorEffect(ShaderLibrary.rainbow(.float(time)))
+                                            }
+                                        }
+                                    }
+                                }
+                            }
                         }
-                    }
-                }
-            }
             
             if gameLogic.isGameOver ?? false {
                 LottieAnimation(name: "GameOver", contentMode: .center, playbackMode: .playing(.toProgress(1, loopMode: .playOnce)))
