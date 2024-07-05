942fb3ce716b67b6be89dbd64ffb6baf7b3632ed
Dave-Ed-Cast <148765897+Dave-Ed-Cast@users.noreply.github.com>
2024-07-05 13:27:41 +0200

    And the same for main view

--- TicTacToe/Views/MainScreen/MainView.swift
+++ TicTacToe/Views/MainScreen/MainView.swift
@@ -1,9 +1,9 @@
 //
-//  ContentView.swift
-//  TicTacToe
-//
-//  Created by Davide Castaldi on 17/05/24.
-//
+    //  ContentView.swift
+    //  TicTacToe
+    //
+    //  Created by Davide Castaldi on 17/05/24.
+    //
 
 import SwiftUI
 
@@ -27,71 +27,70 @@ struct MainView: View {
                         }
                     }
             } else {
-                NavigationView {
-                    VStack(spacing: 10) {
 
-                        VStack {
+                VStack(spacing: 10) {
 
-                            Image("appicon")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(maxHeight: 120)
-                                .cornerRadius(20)
+                    VStack {
 
-                            Text("Tria Tactics")
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                            Text("The game for true tacticians!")
-                                .font(.headline)
-                                .fontWeight(.semibold)
-                        }
-                        .foregroundStyle(.textTheme)
-                        .padding()
-                        .background {
-                            ZStack(alignment: .topTrailing) {
-                                RoundedRectangle(cornerRadius: 20)
-                                    .fill(.buttonTheme.opacity(0.8))
-                                infoButton
-                            }
-                        }
-                        .matchedGeometryEffect(id: "heroRectangle", in: animationNamespace)
-                        .padding(.bottom, 100)
+                        Image("appicon")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxHeight: 120)
+                            .cornerRadius(20)
 
-                        VStack(spacing: 30) {
-                            PrimaryButton(label: "Play", action: {
-                                withAnimation {
-                                    changeViewTo.value = .play
-                                }
-                            })
-                            PrimaryButton(label: "Tutorial", action: {
-                                withAnimation {
-                                    changeViewTo.value = .tutorial
-                                }
-                            })
-                        }// end of 2nd inner vstack
-                    }// end of outer vstack
-
-                    .background {
-                        BackgroundView()
+                        Text("Tria Tactics")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        Text("The game for true tacticians!")
+                            .font(.headline)
+                            .fontWeight(.semibold)
                     }
+                    .foregroundStyle(.textTheme)
+                    .padding()
+                    .background {
+                        ZStack(alignment: .topTrailing) {
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(.buttonTheme.opacity(0.8))
+                            infoButton
+                        }
+                    }
+                    .matchedGeometryEffect(id: "heroRectangle", in: animationNamespace)
+                    .padding(.bottom, 100)
+
+                    VStack(spacing: 30) {
+                        PrimaryButton(label: "Play", action: {
+                            withAnimation {
+                                changeViewTo.value = .play
+                            }
+                        }, color: .buttonTheme.opacity(0.8))
+
+                        PrimaryButton(label: "Tutorial", action: {
+                            withAnimation {
+                                changeViewTo.value = .tutorial
+                            }
+                        }, color: .buttonTheme.opacity(0.8))
+                    }// end of 2nd inner vstack
+                }// end of outer vstack
+
+                .background {
+                    BackgroundView()
                 }
-                .tint(.black)
                 .lineLimit(1)
             }
         }
+
     }
 
     var infoButton: some View {
         Button {
-            showCreditsView = true
+            withAnimation {
+                changeViewTo.value = .collaborators
+            }
         } label: {
             Image(systemName: "info.circle")
                 .foregroundStyle(.buttonTheme)
                 .font(.title3)
                 .colorInvert()
-        }
-        .sheet(isPresented: $showCreditsView) {
-            CreditsView()
         }
         .padding()
     }
